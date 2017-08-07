af8a21ad8719621d39aa7e6af5d28a5afa1def6e
bmdilts <bmdilts@gmail.com>
2017-08-07 00:09:00 -0400

    weapons carousel complete

--- src/components/Weapons.js
+++ src/components/Weapons.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap'
+import {Carousel} from 'react-bootstrap'
 
 export default class Weapons extends React.Component {
   constructor(props) {
@@ -12,123 +12,269 @@ export default class Weapons extends React.Component {
         <Carousel className='cont'>
           <Carousel.Item>
             <div className="item-box">
-                <img alt="Aged leather wrapped bow" src="/data/Weapons/Aged leather wrapped bow.jpg" />
-                <Carousel.Caption>
-                  <h2>Aged leather wrapped bow</h2>
-                  <button><span>Buy Now!</span></button>
-                </Carousel.Caption>
+              <img alt="Aged leather wrapped bow" src="/data/Weapons/Aged leather wrapped bow.jpg"/>
+              <Carousel.Caption>
+                <h2>Aged leather wrapped bow</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
             </div>
           </Carousel.Item>
           <Carousel.Item>
             <div className="item-box">
-                <img alt="Arrows" src="/data/Weapons/arrows.jpeg"/>
-                <Carousel.Caption>
-                  <h2>Arrows</h2>
-                  <button><span>Buy Now!</span></button>
-                </Carousel.Caption>
+              <img alt="Arrows" src="/data/Weapons/arrows.jpeg"/>
+              <Carousel.Caption>
+                <h2>Arrows</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
             </div>
           </Carousel.Item>
           <Carousel.Item>
             <div className="item-box">
-                <img alt="Bone Sword" src="/data/Weapons/bone sword.png"/>
-                <Carousel.Caption>
-                  <h2 style={{color: 'white'}}>Bone Sword</h2>
-                  <button><span>Buy Now!</span></button>
-                </Carousel.Caption>
+              <img alt="Bone Sword" src="/data/Weapons/bone sword.png"/>
+              <Carousel.Caption>
+                <h2 style={{
+                  color: 'white'
+                }}>Bone Sword</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
             </div>
           </Carousel.Item>
           <Carousel.Item>
             <div className="item-box">
-                <img alt="Claymore Helm Pike Combo" src="/data/Weapons/Claymore Helm Pike Combo.jpg" />
-                <Carousel.Caption>
-                  <h2 style={{color: 'white'}}>Claymore Helm Pike Combo</h2>
-                  <button><span>Buy Now!</span></button>
-                </Carousel.Caption>
+              <img alt="Claymore Helm Pike Combo" src="/data/Weapons/Claymore Helm Pike Combo.jpg"/>
+              <Carousel.Caption>
+                <h2 style={{
+                  color: 'white'
+                }}>Claymore Helm Pike Combo</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
             </div>
           </Carousel.Item>
           <Carousel.Item>
             <div className="item-box">
-                <img alt="Crossbow" src="/data/Weapons/Crossbow.jpg" />
-                <Carousel.Caption>
-                  <h2>Crossbow</h2>
-                  <button><span>Buy Now!</span></button>
-                </Carousel.Caption>
+              <img alt="Crossbow" src="/data/Weapons/Crossbow.jpg"/>
+              <Carousel.Caption>
+                <h2>Crossbow</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
             </div>
           </Carousel.Item>
           <Carousel.Item>
             <div className="item-box">
-                <img alt="Crusader Sword" src="/data/Weapons/crusader sword.png" />
-                <Carousel.Caption>
-                  <h2 style={{color: 'white'}}>Crusader Sword</h2>
-                  <button><span>Buy Now!</span></button>
-                </Carousel.Caption>
+              <img alt="Crusader Sword" src="/data/Weapons/crusader sword.png"/>
+              <Carousel.Caption>
+                <h2 style={{
+                  color: 'white'
+                }}>Crusader Sword</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
             </div>
           </Carousel.Item>
           <Carousel.Item>
             <div className="item-box">
-                <img alt="Dark Elf Sword" src="/data/Weapons/dark_angel_2_larp_sword.png" />
-                <Carousel.Caption>
-                  <h2 style={{color: 'white'}}>Dark Elf Sword</h2>
-                  <button><span>Buy Now!</span></button>
-                </Carousel.Caption>
+              <img alt="Dark Elf Sword" src="/data/Weapons/dark_angel_2_larp_sword.png"/>
+              <Carousel.Caption>
+                <h2 style={{
+                  color: 'white'
+                }}>Dark Elf Sword</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
             </div>
           </Carousel.Item>
           <Carousel.Item>
             <div className="item-box">
-                <img alt="Double Bladed Hand Axe" src="/data/Weapons/Dbl bladed hand axe.jpg" />
-                <Carousel.Caption>
-                  <h2 style={{color: 'white'}}>Double Bladed Hand Axe</h2>
-                  <button><span>Buy Now!</span></button>
-                </Carousel.Caption>
+              <img alt="Double Bladed Hand Axe" src="/data/Weapons/Dbl bladed hand axe.jpg"/>
+              <Carousel.Caption>
+                <h2 style={{
+                  color: 'white'
+                }}>Double Bladed Hand Axe</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
             </div>
           </Carousel.Item>
           <Carousel.Item>
             <div className="item-box">
-                <img alt="Scicle Sword" src="/data/Weapons/egyptian scicle sword.jpg" />
-                <Carousel.Caption>
-                  <h2>Scicle Sword</h2>
-                  <button><span>Buy Now!</span></button>
-                </Carousel.Caption>
+              <img alt="Scicle Sword" src="/data/Weapons/egyptian scicle sword.jpg"/>
+              <Carousel.Caption>
+                <h2>Scicle Sword</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
             </div>
           </Carousel.Item>
           <Carousel.Item>
             <div className="item-box">
-                <img alt="Aged leather wrapped bow" src="/data/Weapons/Aged leather wrapped bow.jpg" />
-                <Carousel.Caption>
-                  <h2>Aged leather wrapped bow</h2>
-                  <button><span>Buy Now!</span></button>
-                </Carousel.Caption>
+              <img alt="Mountain Elven Bow" src="/data/Weapons/elven bow.png"/>
+              <Carousel.Caption>
+                <h2 style={{
+                  color: 'white'
+                }}>Mountain Elf Sacred Bow</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
             </div>
           </Carousel.Item>
           <Carousel.Item>
             <div className="item-box">
-                <img alt="Aged leather wrapped bow" src="/data/Weapons/Aged leather wrapped bow.jpg" />
-                <Carousel.Caption>
-                  <h2>Aged leather wrapped bow</h2>
-                  <button><span>Buy Now!</span></button>
-                </Carousel.Caption>
+              <img alt="Gladiator Dragon Sword" src="/data/Weapons/Gladiator Dragon Sword.jpg"/>
+              <Carousel.Caption>
+                <h2>Gladiator Dragon Sword</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
             </div>
           </Carousel.Item>
           <Carousel.Item>
             <div className="item-box">
-                <img alt="Aged leather wrapped bow" src="/data/Weapons/Aged leather wrapped bow.jpg" />
-                <Carousel.Caption>
-                  <h2>Aged leather wrapped bow</h2>
-                  <button><span>Buy Now!</span></button>
-                </Carousel.Caption>
+              <img alt="Dome Splitting Hammer" src="/data/Weapons/hammer.jpg"/>
+              <Carousel.Caption>
+                <h2>Dome Splitting Hammer</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
             </div>
           </Carousel.Item>
           <Carousel.Item>
             <div className="item-box">
-                <img alt="Aged leather wrapped bow" src="/data/Weapons/Aged leather wrapped bow.jpg" />
-                <Carousel.Caption>
-                  <h2>Aged leather wrapped bow</h2>
-                  <button><span>Buy Now!</span></button>
-                </Carousel.Caption>
+              <img alt="Elven Horned Bow" src="/data/Weapons/Horned bow.jpg"/>
+              <Carousel.Caption>
+                <h2>Elven Horned Bow</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="item-box">
+              <img alt="Djinn Sword" src="/data/Weapons/Jian Province Sword.jpg"/>
+              <Carousel.Caption>
+                <h2 style={{
+                  color: 'white'
+                }}>Djinn Sword</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="item-box">
+              <img alt="Triple Katar" src="/data/Weapons/Katar triple.jpg"/>
+              <Carousel.Caption>
+                <h2>Triple Katar</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="item-box">
+              <img alt="Bruiser Mace" src="/data/Weapons/mace.jpg"/>
+              <Carousel.Caption>
+                <h2>Bruiser Mace</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="item-box">
+              <img alt="Mithrodin Sword" src="/data/Weapons/mithrodin sword.png"/>
+              <Carousel.Caption>
+                <h2 style={{
+                  color: 'white'
+                }}>Mithrodin Sword</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="item-box">
+              <img alt="Hunter's Recurve Bow" src="/data/Weapons/Recurve Bow.png"/>
+              <Carousel.Caption>
+                <h2 style={{
+                  color: 'white'
+                }}>Hunter's Recurve Bow</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="item-box">
+              <img alt="Ebony Engraved Claymore" src="/data/Weapons/round hilt engraved claymore.png"/>
+              <Carousel.Caption>
+                <h2 style={{
+                  color: 'white'
+                }}>Ebony Engraved Claymore</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="item-box">
+              <img alt="Elven Scimitar" src="/data/Weapons/scimitar.jpg"/>
+              <Carousel.Caption>
+                <h2>Elven Scimitar</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="item-box">
+              <img alt="Berzerker Axe" src="/data/Weapons/Viking axe.jpg"/>
+              <Carousel.Caption>
+                <h2>Berzerker Axe</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="item-box">
+              <img alt="Rhohan Yew Bow" src="/data/Weapons/yew bow.jpg"/>
+              <Carousel.Caption>
+                <h2 style={{
+                  color: 'white'
+                }}>Rhohan Yew Bow</h2>
+                <button>
+                  <span>Buy Now!</span>
+                </button>
+              </Carousel.Caption>
             </div>
           </Carousel.Item>
         </Carousel>
-
       </div>
     )
   }
